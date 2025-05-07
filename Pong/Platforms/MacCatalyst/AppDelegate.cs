a0258f418fcee7c379b443f1458fce956c26b97d
Josh Given <josh.given@christianacare.org>
2025-05-07 19:01:51 -0400

    Add project files.

---
+++ Pong/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace Pong;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
